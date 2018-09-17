4adc3b0424e56ea712684a0989d3141561ea4b34
robynph <robyn@redh.io>
2018-09-16 21:07:41 -0400

    updated main.py

--- api_main.py
+++ api_main.py
@@ -3,6 +3,7 @@
 Created on Mon Sep 10 18:24:13 2018
 
 @author: Monika Asawa
+@edits by RH
 """
 from flask import Flask, request, jsonify
 
@@ -16,8 +17,20 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 @app.route("/test", methods=['GET'])
-def loadStoreData():
+def test():
 
     if request.method == 'GET':
+            return ("This is a test :) ")
 
-            return ("Received value of request :: ")
+@app.route("/transDetailGET", methods=['GET'])
+def transDetailGET():
+
+    if request.method == 'GET':
+            return jsonify({"app_id” : “App id1”, “model_id” : “Model id1”, “Date” : “Sept. 16, 2018”, “order_id” : “2348858”, “trans_id” : “0xtransID”, “selected_product” : “sku1”, “bundled_products” : “order line item sku”, “Original Revnue” : “$####”, “Total Revenue” : “$#####”}
+)
+
+@app.route("/regDetailGET", methods=['GET'])
+def regDetailGET():
+
+    if request.method == 'GET':
+            return jsonify({"app_id” : “App id1”, “walletID” : “0xwallet”, “transID” : “0xtransiD”, “Total Revenue” : “$###,###”, “Total Bundles Generated” : “$###,###”})
